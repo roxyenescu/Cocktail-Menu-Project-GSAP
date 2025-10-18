b4e28004aadd2498e24b81d4fd0aa72e2b906a5f
roxyenescu <115460507+roxyenescu@users.noreply.github.com>
2025-10-18 17:17:49 +0300

    Fixed design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -43,9 +43,8 @@ const Hero = () => {
             .to('.right-leaf', { y: 200 }, 0)
             .to('.left-leaf', { y: -200 }, 0)
 
-
         const startValue = isMobile ? 'top 50%' : 'center 60%';
-        const endValue = isMobile ? '120% top' : 'bottom value';
+        const endValue = isMobile ? '170% top' : 'bottom top';
 
         // Video animation timeline
         const tl = gsap.timeline({
